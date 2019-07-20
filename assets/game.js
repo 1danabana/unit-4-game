278f3bf23da952e4c4ad7ee2164742f4624a32dd
unknown <1danabana@gmail.com>
2019-07-19 19:36:00 -0700

    adding the frame work of dynamically adding html through js

--- assets/game.js
+++ assets/game.js
@@ -1,4 +1,14 @@
+var hpbar = [200, 230]
+for (var i = 0; i<hpbar.length; i++){
+  
+  //dynamic character selctor!
+  var char = $('<img>');
+  char.addclass('fighter');
+  char.attr("src", "https://vignette.wikia.nocookie.net/guilty-gear/images/d/de/Jam_Xrd.png/revision/latest/scale-to-width-down/337?cb=20160526230307")
+}
+
+//button to use for manipulating player character
 $("#object1").on("click", function() {
-    alert("I've been clicked!");
+    
   });
-  console.log(alert)
+  
