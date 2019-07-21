7b3540a9befc2ff37a3493a03999e373a2f68700
unknown <1danabana@gmail.com>
2019-07-20 19:39:56 -0700

    im trying to use .push to add all the images to a single function

--- assets/game.js
+++ assets/game.js
@@ -1,14 +1,23 @@
-var hpbar = [200, 230]
-for (var i = 0; i<hpbar.length; i++){
+// function to display all the selectable characters
+function characters(){
+  var fighters = [];
   
-  //dynamic character selctor!
-  var char = $('<img>');
-  char.addclass('fighter');
-  char.attr("src", "https://vignette.wikia.nocookie.net/guilty-gear/images/d/de/Jam_Xrd.png/revision/latest/scale-to-width-down/337?cb=20160526230307")
-}
+  // fighters to choose from
+  fighters.push("https://vignette.wikia.nocookie.net/guilty-gear/images/d/de/Jam_Xrd.png/revision/latest/scale-to-width-down/337?cb=20160526230307");
+  fighters.push("https://vignette.wikia.nocookie.net/guilty-gear/images/6/60/Kliff_Underson.png/revision/latest/scale-to-width-down/350?cb=20111210045438");
+  fighters.push("https://vignette.wikia.nocookie.net/guilty-gear/images/b/b2/Sol_image_rev.png/revision/latest/scale-to-width-down/350?cb=20160526181730");
+ 
+  // a loop that posts all the imgaes
+  for(var i = 0; fighters.length > i; i++){
+    var img = new Image(200, 200);
+      img.src = fighters[i];
 
-//button to use for manipulating player character
-$("#object1").on("click", function() {
-    
-  });
+      var src = document.getElementById("#choices");
+      src.append(img);
+  }
+}
+  console.log(characters)
+  
+
+  
   
